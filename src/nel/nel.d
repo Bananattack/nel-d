23776a02354d4e82fbad1b2399b20873c0a3e3e9
Ted Reed <ted.reed@gmail.com>
2011-08-16 21:47:04 -0700

    Add a default case for options not starting with a dash (equiv to ArgumentState.INPUT)

--- src/nel/nel.d
+++ src/nel/nel.d
@@ -88,6 +88,7 @@ void main(string[] arguments)
         {
             switch(state)
             {
+                default:
                 case ArgumentState.INPUT:
                     
                     if(input != "")
